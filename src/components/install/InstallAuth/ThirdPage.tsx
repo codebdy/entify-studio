1a0e20d5f46ac02e9624eff27d5ae542dd5b2825
Water.Li <lizongbin@hotmail.com>
2022-05-14 17:24:45 +0800

    Update ThirdPage.tsx

--- src/components/install/InstallAuth/ThirdPage.tsx
+++ src/components/install/InstallAuth/ThirdPage.tsx
@@ -44,7 +44,7 @@ export const ThirdPage = memo(
       [onUrlChange]
     );
     const handleFinish = () => {
-      service && register({ ...service, serviceType: ServiceType.auth });
+      service && register({ ...service, name, url, serviceType: ServiceType.auth });
     };
 
     const err = error || addError;
