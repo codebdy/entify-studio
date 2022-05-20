97ce379e4a7102d1452c40606803c3d25ac57fe1
Water.Li <lizongbin@hotmail.com>
2022-05-20 12:54:30 +0800

    Update useValidate.ts

--- src/components/ModelBoard/hooks/useValidate.ts
+++ src/components/ModelBoard/hooks/useValidate.ts
@@ -7,25 +7,26 @@ import { useGetClassAssociations } from "./useGetClassAssociations";
 import intl from "react-intl-universal";
 
 function hasDuplicates(array: string[]) {
-  return _.some(array, function(elt, index) { 
-      return array.indexOf(elt) !== index; 
+  return _.some(array, function (elt, index) {
+    return array.indexOf(elt) !== index;
   });
 }
 
-
 export function useValidate(serviceId: number) {
   const classes = useRecoilValue(classesState(serviceId));
-  const alertError = useAlertError()
+  const alertError = useAlertError();
   const getClassAssociations = useGetClassAssociations(serviceId);
   const validate = useCallback(() => {
     //检查属性名重复
-    for (const cls of classes){
-      const names = cls.attributes.map(atr=>atr.name)
-      names.push(...cls.methods.map(mth=>mth.name))
-      names.push(...getClassAssociations(cls.uuid).map(aso=>aso.name))
-      if(hasDuplicates(names.filter(name=>!!name))){
-        alertError(intl.get("duplicated-property-error", {cls:cls.name}))
-        return false
+    for (const cls of classes) {
+      const names = cls.attributes?.map((atr) => atr.name) || [];
+      names.push(...(cls.methods?.map((mth) => mth.name) || []));
+      names.push(
+        ...(getClassAssociations(cls.uuid)?.map((aso) => aso.name) || [])
+      );
+      if (hasDuplicates(names.filter((name) => !!name))) {
+        alertError(intl.get("duplicated-property-error", { cls: cls.name }));
+        return false;
       }
     }
     //检查关联类属性名冲突
