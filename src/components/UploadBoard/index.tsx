d709b89d86745f41fb9302ea40b1a80ae2ee928f
Water.Li <lizongbin@hotmail.com>
2022-06-28 17:08:31 +0800

    Update index.tsx

--- src/components/UploadBoard/index.tsx
+++ src/components/UploadBoard/index.tsx
@@ -1,10 +1,22 @@
-import { Container } from "@mui/material"
+import { Button, Container, Stack, styled } from "@mui/material"
 import { memo } from "react"
+import intl from "react-intl-universal"
+
+const Input = styled('input')({
+  display: 'none',
+});
 
 export const UploadBoard = memo(() => {
   return (
-    <Container>
-      
+    <Container maxWidth ="xl">
+      <Stack direction="row" alignItems="center" spacing={2} sx={{ mt: 2 }}>
+        <label htmlFor="contained-button-file">
+          <Input accept="image/*" id="contained-button-file" multiple type="file" />
+          <Button variant="contained" component="span">
+            {intl.get("file-upload")}
+          </Button>
+        </label>
+      </Stack>
     </Container>
   )
 })
