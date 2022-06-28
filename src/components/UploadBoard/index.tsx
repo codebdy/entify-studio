7293c7bc027c0e28215e7120503a68b4352e31a5
Water.Li <lizongbin@hotmail.com>
2022-06-28 21:59:33 +0800

    Update index.tsx

--- src/components/UploadBoard/index.tsx
+++ src/components/UploadBoard/index.tsx
@@ -14,29 +14,34 @@ const gql = `
 
 export const UploadBoard = memo(() => {
   const [name, setName] = useState("")
+  const [file, setFile] = useState<File>()
   const createClient = useCreateGQLClient();
   const theme = useTheme()
-  const handleUpload = useCallback(
+  const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const files = event.target.files
       if (files?.length) {
         setName(files[0].name)
+        setFile(files[0])
       }
-      console.log(files)
     },
-    []
+    [setFile]
   );
+
+  const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>)=>{
+    setName(event.target.value)
+  }, [])
 
 
   return (
     <Container maxWidth="xl">
-      <Box sx={{mt:2}}>
-        <input accept="image/*" type="file" onChange={handleUpload} style={{ color: theme.palette.text.secondary }} />
+      <Box sx={{ mt: 2 }}>
+        <input accept="image/*" type="file" onChange={handleFileChange} style={{ color: theme.palette.text.secondary }} />
       </Box>
 
       <Stack direction="row" alignItems="center" spacing={2} sx={{ mt: 2 }}>
-        <TextField label={intl.get("name")} variant="outlined" size="small" value={name} />
-        <LoadingButton variant="contained" component="span">
+        <TextField label={intl.get("name")} variant="outlined" size="small" value={name} onChange ={handleNameChange}/>
+        <LoadingButton variant="contained" component="span" disabled={!file}>
           {intl.get("upload")}
         </LoadingButton>
       </Stack>
