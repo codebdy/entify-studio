e20dee087442bf079a246e76d62d9acaa84c6f05
Water.Li <lizongbin@hotmail.com>
2022-06-28 22:19:20 +0800

    Update index.tsx

--- src/components/UploadBoard/index.tsx
+++ src/components/UploadBoard/index.tsx
@@ -1,8 +1,13 @@
 import { LoadingButton } from "@mui/lab";
-import { Button, Container, Stack, styled, TextField, useTheme, Box } from "@mui/material"
+import { Container, Stack, TextField, useTheme, Box } from "@mui/material"
 import { useCreateGQLClient } from "do-ents/useCreateGQLClient";
 import { memo, useCallback, useState } from "react"
 import intl from "react-intl-universal"
+import { useSelectedService } from 'components/ModelBoard/hooks/useSelectedService';
+import { ServerError } from "do-ents/ServerError";
+import { ClientError } from "graphql-request";
+import { parseErrorMessage } from "do-ents/parseErrorMessage";
+import { useShowServerError } from 'hooks/useShowServerError';
 
 const gql = `
   mutation upload($file:Upload!, $name:string){
@@ -15,8 +20,15 @@ const gql = `
 export const UploadBoard = memo(() => {
   const [name, setName] = useState("")
   const [file, setFile] = useState<File>()
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<ServerError | undefined>();
+  
   const createClient = useCreateGQLClient();
   const theme = useTheme()
+  const service = useSelectedService()
+
+  useShowServerError(error)
+
   const handleFileChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const files = event.target.files
@@ -32,6 +44,25 @@ export const UploadBoard = memo(() => {
     setName(event.target.value)
   }, [])
 
+  const handleUpload = useCallback(()=>{
+    const graphQLClient = createClient(service?.url);
+
+    graphQLClient.setHeader("Content-Type", "multipart/form-data")
+    setLoading(true);
+    setError(undefined);
+    graphQLClient
+      .request(gql, { file, name })
+      .then((data) => {
+        setLoading(false);
+      })
+      .catch((err: ClientError) => {
+        const message = parseErrorMessage(err);
+        setLoading(false);
+        const serverError:ServerError = { message: message, serverUrl:service?.url }
+        setError(serverError);
+        console.error(err);
+      });
+  }, [createClient, file, name, service?.url])
 
   return (
     <Container maxWidth="xl">
@@ -41,7 +72,7 @@ export const UploadBoard = memo(() => {
 
       <Stack direction="row" alignItems="center" spacing={2} sx={{ mt: 2 }}>
         <TextField label={intl.get("name")} variant="outlined" size="small" value={name} onChange ={handleNameChange}/>
-        <LoadingButton variant="contained" component="span" disabled={!file}>
+        <LoadingButton variant="contained" loading = {loading} component="span" disabled={!file} onClick = {handleUpload}>
           {intl.get("upload")}
         </LoadingButton>
       </Stack>
