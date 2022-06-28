0fcc9403913bb74d1f46e55a113e207045dcafa0
Water.Li <lizongbin@hotmail.com>
2022-06-28 17:15:03 +0800

    Update index.tsx

--- src/components/UploadBoard/index.tsx
+++ src/components/UploadBoard/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Container, Stack, styled } from "@mui/material"
+import { useCreateGQLClient } from "do-ents/useCreateGQLClient";
 import { memo } from "react"
 import intl from "react-intl-universal"
 
@@ -6,9 +7,19 @@ const Input = styled('input')({
   display: 'none',
 });
 
+const gql = `
+  mutation upload($file:Upload!, $name:string){
+    singleUpload(file:$file, name:$name){
+      id
+    }
+  }
+`
+
 export const UploadBoard = memo(() => {
+  const createClient = useCreateGQLClient();
+
   return (
-    <Container maxWidth ="xl">
+    <Container maxWidth="xl">
       <Stack direction="row" alignItems="center" spacing={2} sx={{ mt: 2 }}>
         <label htmlFor="contained-button-file">
           <Input accept="image/*" id="contained-button-file" multiple type="file" />
