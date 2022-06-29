2a5d6e063145d9181fd0ddbb4d530b024e40634a
Water.Li <lizongbin@hotmail.com>
2022-06-30 07:59:05 +0800

    Update index.tsx

--- src/components/UploadBoard/index.tsx
+++ src/components/UploadBoard/index.tsx
@@ -1,5 +1,5 @@
 import { LoadingButton } from "@mui/lab";
-import { Container, Stack, TextField, useTheme, Box } from "@mui/material"
+import { Container, TextField, useTheme, Grid } from "@mui/material"
 import { memo, useCallback, useState } from "react"
 import intl from "react-intl-universal"
 import { useSelectedService } from 'components/ModelBoard/hooks/useSelectedService';
@@ -23,7 +23,7 @@ export const UploadBoard = memo(() => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<ServerError | undefined>();
   const token = useToken();
-  
+
   const theme = useTheme()
   const service = useSelectedService()
 
@@ -40,21 +40,21 @@ export const UploadBoard = memo(() => {
     [setFile]
   );
 
-  const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>)=>{
+  const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
   }, [])
 
-  const handleUpload = useCallback(()=>{
-    if (!service?.url){
-      return 
+  const handleUpload = useCallback(() => {
+    if (!service?.url) {
+      return
     }
-    const client = new AwesomeGraphQLClient({ endpoint: service.url})
+    const client = new AwesomeGraphQLClient({ endpoint: service.url })
     //graphQLClient.setHeader()
     //    client.setHeader(AUTHORIZATION, token ? `${TOKEN_PREFIX}${token}` : "");
     setLoading(true);
     setError(undefined);
     client
-      .request(gql, { file, name }, {headers: {[AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` : ""}})
+      .request(gql, { file, name }, { headers: { [AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` : "" } })
       .then((data) => {
         setLoading(false);
       })
@@ -66,17 +66,29 @@ export const UploadBoard = memo(() => {
   }, [file, name, service?.url, token])
 
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ mt: 2 }}>
-        <input accept="image/*" type="file" onChange={handleFileChange} style={{ color: theme.palette.text.secondary }} />
-      </Box>
-
-      <Stack direction="row" alignItems="center" spacing={2} sx={{ mt: 2 }}>
-        <TextField label={intl.get("name")} variant="outlined" size="small" value={name} onChange ={handleNameChange}/>
-        <LoadingButton variant="contained" loading = {loading} component="span" disabled={!file} onClick = {handleUpload}>
-          {intl.get("upload")}
-        </LoadingButton>
-      </Stack>
+    <Container maxWidth="md">
+      <Grid container spacing={2} sx={{ p: 2 }}>
+        <Grid item xs={12}>
+          <input accept="image/*" type="file" onChange={handleFileChange} style={{ color: theme.palette.text.secondary }} />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label={"Mutation"}
+            variant="outlined"
+            multiline
+            rows={12}
+            size="small"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <LoadingButton variant="contained" loading={loading} component="span" disabled={!file} onClick={handleUpload}>
+            {intl.get("upload")}
+          </LoadingButton>
+        </Grid>
+      </Grid>
     </Container>
   )
 })
